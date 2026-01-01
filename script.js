240d235dce0f96012f041ce77d79d81f4550bdc0
ams2008ahmed-dev <ams2008ahmed@gmail.com>
2026-01-02 00:18:20 +0300

    Implement basic HTTP server with file serving

---
+++ script.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+  
+  if (ext === '.css') contentType = 'text/css';
+  if (ext === '.js') contentType = 'text/javascript';
+  if (ext === '.json') contentType = 'application/json';
+  if (ext === '.png') contentType = 'image/png';
+  if (ext === '.jpg' || ext === '.jpeg') contentType = 'image/jpeg';
+  if (ext === '.gif') contentType = 'image/gif';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not Found');
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(5000, '0.0.0.0', () => {
+  console.log('Server running on port 5000');
+});
